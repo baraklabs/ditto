3d93a49ff7d35bb324ec61a8f6d5605b5ed6d0e1
Yashmusk <agarwalyash541@gmail.com>
2025-06-15 20:32:44 +0530

    feat: added my feature

--- ui/src/components/Footer.jsx
+++ ui/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+//footer component
 const Footer = () => (
   <footer className="bg-gray-800 text-white p-4 text-center">
     <p>&copy; 2025 Baraklabs. All Rights Reserved.</p>
